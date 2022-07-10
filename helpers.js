8b62ffdb8f55673950e474c12cdca0dd6eb4c521
inomesh <nomesh.sehgal@gmail.com>
2022-07-11 02:52:18 +0530

    fixes for new release

--- helpers.js
+++ helpers.js
@@ -1,14 +1,59 @@
 const excelToJson = require("convert-excel-to-json");
 const fs = require("fs");
 
+/**
+ * @description
+ * In case all objects in an array of objects doesn't have some missing keys then this function will append those missing keys in all other objects and make it equal
+ */
+function handleMakeEqual(arr) {
+  const keys = arr.reduce(
+    (acc, curr) => (Object.keys(curr).forEach((key) => acc.add(key)), acc),
+    new Set()
+  );
+
+  const output = arr.map((item) =>
+    [...keys].reduce((acc, key) => ((acc[key] = item[key] ?? ""), acc), {})
+  );
+  return output;
+}
+
 function filter(data = []) {
-  const header = data[0];
-  const body = data.slice(1);
+  const arr = handleMakeEqual(data);
+  const header = arr[0];
+  const body = arr.slice(1);
   const raw = body.map((item) => {
     const instance = {};
     for (const key in item) {
-      instance[header[key]] =
-        item[key] === null && item[key] === undefined ? "" : item[key];
+      let newInstanceKey = header[key];
+      if (newInstanceKey in instance) {
+        // already another key exists in instance
+        const reg = new RegExp(newInstanceKey, "gi");
+        const noOfOccurence = Object.keys(instance).filter((item) =>
+          reg.test(item)
+        ).length;
+        newInstanceKey = `${newInstanceKey}-${noOfOccurence}`;
+        instance[newInstanceKey] = item[key];
+      }
+
+      if (
+        /Invoice Date/gi.test(newInstanceKey) ||
+        /Cancellation Date/gi.test(newInstanceKey)
+      ) {
+        // date validation
+        if (!item[key]) {
+          instance[newInstanceKey] = "";
+        } else if (new Date(item[key]) == "Invalid Date") {
+          instance[newInstanceKey] = item[key];
+        } else {
+          instance[newInstanceKey] = new Date(item[key]).toLocaleDateString();
+        }
+      } else if (item[key] || item[key] === 0) {
+        // item[key] has a zero as value or some value
+        instance[newInstanceKey] = item[key];
+      } else {
+        // item[key] is null or undefined
+        instance[newInstanceKey] = "";
+      }
     }
     return instance;
   });
--- index.js
+++ index.js
@@ -42,50 +42,64 @@ app.post(
   "/upload",
   upload.fields([
     { name: "master", maxCount: 1 },
-    { name: "append", maxCount: 5 },
+    { name: "append", maxCount: 10 },
   ]),
   (req, res) => {
-    if (Object.keys(req.files)?.length !== 2) {
-      return res.redirect("/");
-    }
-
-    const masterFilePath = req.files.master[0].path;
-    const appendFilesPathArr = req.files.append.map((file) => file.path);
-
-    const files = [masterFilePath, appendFilesPathArr].flat();
-    const arr = [];
-    for (let index = 0; index < files.length; index++) {
-      const obj = ExcToJSON(files[index]);
-      arr.push(obj);
-    }
-    const obj = {};
-    arr.map((item) => {
-      for (let key in item) {
-        obj[key] = obj[key] ? [...obj[key], ...item[key]] : [...item[key]];
+    try {
+      if (Object.keys(req.files)?.length !== 2) {
+        return res.redirect("/");
       }
-    });
 
-    const xls = json2xls(Object.values(obj).flat());
+      const masterFilePath = req.files.master[0].path;
+      const appendFilesPathArr = req.files.append.map((file) => file.path);
 
-    const finalFile = path.join(__dirname, "result", `final.xlsx`);
-    fs.writeFileSync(finalFile, xls, "binary");
-
-    res.sendFile(finalFile);
-
-    setTimeout(() => {
-      fs.rmSync(path.join(__dirname, "result"), {
-        recursive: true,
-        force: true,
+      const files = [masterFilePath, appendFilesPathArr].flat();
+      const arr = [];
+      for (let index = 0; index < files.length; index++) {
+        const obj = ExcToJSON(files[index]);
+        arr.push(obj);
+      }
+      const obj = {};
+      arr.map((item) => {
+        for (let key in item) {
+          obj[key] = obj[key] ? [...obj[key], ...item[key]] : [...item[key]];
+        }
       });
-      fs.rmSync(path.join(__dirname, "uploads"), {
-        recursive: true,
-        force: true,
-      });
-      fs.mkdirSync(path.join(__dirname, "result"));
-      fs.mkdirSync(path.join(__dirname, "uploads"));
-    }, 100);
+
+      const xls = json2xls(Object.values(obj).flat());
+
+      const finalFile = path.join(__dirname, "result", `final.xlsx`);
+      fs.writeFileSync(finalFile, xls, "binary");
+
+      res.sendFile(finalFile);
+    } catch (error) {
+      console.error(error.message);
+      res.redirect("/error");
+    } finally {
+      // clean up
+      setTimeout(() => {
+        fs.rmSync(path.join(__dirname, "result"), {
+          recursive: true,
+          force: true,
+        });
+        fs.rmSync(path.join(__dirname, "uploads"), {
+          recursive: true,
+          force: true,
+        });
+        fs.mkdirSync(path.join(__dirname, "result"));
+        fs.mkdirSync(path.join(__dirname, "uploads"));
+      }, 100);
+    }
   }
 );
+
+app.get("/error", (req, res) => {
+  res.render("pages/500");
+});
+
+app.get("/*", (req, res) => {
+  res.render("pages/404");
+});
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
